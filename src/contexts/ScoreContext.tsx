e4643f630c35e0e0a5b00ffeadd0e1819bbfae07
vickbk <vickbake8@gmail.com>
2025-12-21 14:20:27 +0200

    inital routing setup

--- src/contexts/ScoreContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-type ScoreType = [number, (num: number) => void];
-export const ScoreContext = createContext([
-  0,
-  (num: number) => {
-    console.log(num);
-  },
-] as ScoreType);
