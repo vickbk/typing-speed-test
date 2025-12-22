68892b2134534c868cda4e37ef828caffb1bd708
vickbk <vickbake8@gmail.com>
2025-12-22 10:40:08 +0200

    updating challenge mode and options

--- src/components/app/editor/settings/challenge-mode.tsx
+++ src/components/app/editor/settings/challenge-mode.tsx
@@ -1,3 +1,29 @@
+import { Article } from "../../../shared/Article";
+import { Heading } from "../../../shared/Heading";
+import { SROnly } from "../../../shared/SROnly";
+
 export const ChallengeMode = () => {
-  return <div>ChallengeMode</div>;
+  return (
+    <Article>
+      <Heading>
+        <SROnly>Select your challenge </SROnly>Mode:
+      </Heading>
+      <ul>
+        {[15, 30, 60, 120, ""].map((value) => (
+          <li key={value}>
+            <label>
+              <input
+                type="radio"
+                name="mode"
+                value={value}
+                defaultChecked
+                className="sr-only"
+              />{" "}
+              {value !== "" ? `Timed (${value}s)` : "Passage"}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </Article>
+  );
 };
--- src/components/app/editor/settings/challenge-options.tsx
+++ src/components/app/editor/settings/challenge-options.tsx
@@ -1,3 +1,29 @@
+import { Article } from "../../../shared/Article";
+import { Heading } from "../../../shared/Heading";
+import { SROnly } from "../../../shared/SROnly";
+
 export const ChallengeOptions = () => {
-  return <div>ChallengeOptions</div>;
+  return (
+    <Article>
+      <Heading>
+        <SROnly>Select your </SROnly>Difficulty<SROnly> level</SROnly>:
+      </Heading>
+      <ul>
+        {["easy", "medium", "hard"].map((level) => (
+          <li key={level}>
+            <label>
+              <input
+                type="radio"
+                name="level"
+                value={level}
+                defaultChecked
+                className="sr-only"
+              />{" "}
+              {level}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </Article>
+  );
 };
