89c1c213623e3127811c9cd01d42e5f427f8297a
vickbk <vickbake8@gmail.com>
2025-12-25 20:27:57 +0200

    styling the start dialog

--- src/components/app/editor/edit-space/start-edit.tsx
+++ src/components/app/editor/edit-space/start-edit.tsx
@@ -4,13 +4,16 @@ import { SROnly } from "../../../shared/SROnly";
 
 export const StartEdit = () => {
   return (
-    <Article className="absolute inset-0 flex flex-col gap-4 justify-center items-center backdrop-blur-xs isolate">
+    <Article className="absolute inset-0 flex flex-col gap-4 justify-center items-center text-center backdrop-blur-xs isolate">
       <Heading>
-        <button type="button" className="p-4">
+        <button
+          type="button"
+          className="p-4 px-8 blue-600 rounded-xl text-2xl font-normal"
+        >
           Start Typing Text
         </button>
       </Heading>
-      <p>
+      <p className="text-xl">
         Or <SROnly>You can</SROnly> click the text and start typing
       </p>
     </Article>
