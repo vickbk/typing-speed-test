8468fbc2abf744005e6efb086b3f95119163b2b8
vickbk <vickbake8@gmail.com>
2026-01-16 16:50:03 +0200

    updating the large logo to use svg to update to the color on light themes

--- src/components/app/header/header.tsx
+++ src/components/app/header/header.tsx
@@ -1,17 +1,19 @@
 import { PersonalBest } from "./personal-best";
 import { default as logoSmall } from "@assets/images/logo-small.svg";
-import { default as logoLarge } from "@assets/images/logo-large.svg";
 import { Heading } from "@/components/shared/Heading";
 import { SROnly } from "@/components/shared/SROnly";
+import { LogoLarge } from "./logo-large";
 
 export const Header = () => {
   return (
     <header className="flex justify-between items-center py-4 w-full max-w-300 m-auto">
       <Heading>
-        <picture>
-          <source media="(min-width: 576px)" srcSet={logoLarge} />
+        <picture className="sm:hidden">
           <img src={logoSmall} alt="" />
         </picture>
+        <div className="hidden sm:block">
+          <LogoLarge />
+        </div>
         <SROnly>
           Typing speed test. Type as fast as you can in 60 seconds
         </SROnly>
