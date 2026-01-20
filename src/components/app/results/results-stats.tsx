24320b9b95d2959833aa0ad9232288eb0efaa0a5
vickbk <vickbake8@gmail.com>
2026-01-20 16:46:49 +0200

    updating results stats structure

--- src/components/app/results/results-stats.tsx
+++ src/components/app/results/results-stats.tsx
@@ -5,40 +5,34 @@ import type { AppState } from "@/libs/types/typing-speed-types";
 
 export default function ResultsStats({ state }: { state: AppState }) {
   const [WPM, accuracy] = [calculateWPM, calculateAccuracy].map((func) =>
-    func(state)
+    func(state),
   );
   const { errorCount, input = "" } = state;
 
   const stats = [
-    {
-      title: (
-        <>
-          <WPMText />:
-        </>
-      ),
-      content: WPM,
-    },
-    {
-      title: "Accuracy:",
-      content: (
-        <span className={accuracy !== "100" ? "c-red-500" : ""}>
-          {accuracy}%
-        </span>
-      ),
-    },
-    {
-      title: "Characters",
-      content: (
-        <span className="c-neutral-400">
-          <span className="c-green-500">{input.length}</span>/
-          <span className="c-red-500">{errorCount}</span>
-        </span>
-      ),
-    },
+    [
+      <>
+        <WPMText />:
+      </>,
+      WPM,
+    ],
+    [
+      "Accuracy:",
+      <span className={accuracy !== "100" ? "c-red-500" : ""}>
+        {accuracy}%
+      </span>,
+    ],
+    [
+      "Characters",
+      <span className="c-neutral-400">
+        <span className="c-green-500">{input.length}</span>/
+        <span className="c-red-500">{errorCount}</span>
+      </span>,
+    ],
   ];
   return (
     <section className="grid gap-4 md:grid-cols-3">
-      {stats.map(({ title, content }, key) => (
+      {stats.map(([title, content], key) => (
         <ResultsShow title={title} key={key}>
           {content}
         </ResultsShow>
