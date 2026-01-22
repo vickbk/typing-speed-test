553224615d205ff09f68de2296275a2183bc319c
vickbk <vickbake8@gmail.com>
2026-01-22 10:26:12 +0200

    aligning the parameters options for large screens

--- src/components/app/editor/settings/challenge-params.tsx
+++ src/components/app/editor/settings/challenge-params.tsx
@@ -20,7 +20,7 @@ export const ChallengeParams = <T extends string | number>({
   const allOptions = { ...other, options: normalizedOptions };
 
   return (
-    <div className="capitalize grow md:flex gap-2 items-center not-last:md:border-r">
+    <div className="capitalize grow lg:grow-0 md:flex gap-2 items-center not-last:md:border-r not-last:md:pr-4">
       <dt className="c-secondary">{children}</dt>
       <dd className="overflow-x-clip md:overflow-x-auto">
         <MobileMenue {...allOptions} />
