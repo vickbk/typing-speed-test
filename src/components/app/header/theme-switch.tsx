73d491bb0b79b0bce05a858a7cad3893b660b4b8
vickbk <vickbake8@gmail.com>
2026-01-22 09:45:06 +0200

    moving theme switching logic to a custom hook

--- src/components/app/header/theme-switch.tsx
+++ src/components/app/header/theme-switch.tsx
@@ -1,34 +1,9 @@
 import { Icon } from "@/components/common/bi-icon";
 import { SROnly } from "@/components/shared/SROnly";
-import getMemoItem from "@/libs/memorization/get-item";
-import setMemoItem from "@/libs/memorization/set-item";
-import { useCallback, useRef, useState } from "react";
+import { useTheme } from "@/hooks/handle-theme";
 
 export const ThemeSwitch = () => {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
-
-  const firstLoad = useRef(true);
-
-  const loadTheme = useCallback(
-    (node: HTMLDivElement | null) => {
-      if (node) {
-        if (firstLoad.current) {
-          firstLoad.current = false;
-          let savedTheme = getMemoItem<"light" | "dark" | undefined>("theme");
-          if (!savedTheme)
-            savedTheme = window.matchMedia("(prefers-color-scheme: dark)")
-              .matches
-              ? "dark"
-              : "light";
-          setTheme(savedTheme!);
-        } else {
-          document.documentElement.setAttribute("theme", theme);
-          setMemoItem("theme", theme);
-        }
-      }
-    },
-    [theme],
-  );
+  const { theme, setTheme, loadTheme } = useTheme();
   return (
     <div className="ml-auto" ref={loadTheme}>
       <button
