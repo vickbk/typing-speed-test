c8693c06d23628fb32faa05a2e9b0da8a9cd5646
vickbk <vickbake8@gmail.com>
2026-03-07 10:42:47 +0200

    refactor: reusing isDark variable in toggleTheme function

--- src/features/theme/hooks/use-theme.ts
+++ src/features/theme/hooks/use-theme.ts
@@ -6,21 +6,22 @@ import type { Themes } from "../types";
 export function useTheme() {
   const [theme, setTheme] = useState<Themes>("light");
 
+  const isDark = theme === "dark";
+
   const changeTheme = useCallback((theme: Themes) => {
     setTheme(theme);
     applyTheme(theme);
   }, []);
 
+  const toggleTheme = useCallback(() => {
+    const nextTheme: Themes = isDark ? "light" : "dark";
+    setMemoItem("theme", nextTheme);
+    changeTheme(nextTheme);
+  }, [isDark]);
+
   useEffect(() => {
     changeTheme(getSavedTheme());
   }, []);
 
-  return {
-    toggleTheme: useCallback(() => {
-      const nextTheme: Themes = theme === "dark" ? "light" : "dark";
-      setMemoItem("theme", nextTheme);
-      changeTheme(nextTheme);
-    }, [theme]),
-    isDark: theme === "dark",
-  };
+  return { isDark, toggleTheme };
 }
