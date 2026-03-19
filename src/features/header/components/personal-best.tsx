37b8ddaf9fd4432aebda50531186ea52d774a446
vickbk <vickbake8@gmail.com>
2026-03-19 14:27:06 +0200

    refactor: initializing header feature

---
+++ src/features/header/components/personal-best.tsx
@@ -0,0 +1,57 @@
+import { Icon } from "@/components/common/bi-icon";
+import { WPMText } from "@/components/common/wpm-text";
+import { SROnly } from "@/components/shared/SROnly";
+import { TypingContext } from "@/contexts/TypingContext";
+import type { TypeScore } from "@/libs/types/typing-speed-types";
+import { getMemoItem } from "@/shared";
+import { default as bestIcon } from "@assets/images/icon-personal-best.svg";
+import { useCallback, useContext } from "react";
+import { Link } from "react-router-dom";
+
+export const PersonalBest = () => {
+  const {
+    state: { difficulty, best },
+    dispatch,
+  } = useContext(TypingContext);
+
+  const loadResults = useCallback(
+    (node: HTMLElement | null) => {
+      if (node !== null) {
+        const results = getMemoItem<TypeScore[]>(`score.${difficulty}`) || [
+          { wpm: 0 },
+        ];
+        const [higher] = results.sort(
+          ({ wpm: aWPM }, { wpm: bWPM }) => bWPM - aWPM,
+        );
+        dispatch({ action: "updateHighScore", payload: higher.wpm });
+      }
+    },
+    [difficulty, best],
+  );
+
+  return (
+    <dl className="best" ref={loadResults}>
+      <dt className="capitalize c-secondary flex gap-1">
+        <img src={bestIcon} alt="" />
+        <span className="sr-only sm:not-sr-only">Personal </span>
+        Best:
+      </dt>
+      <dd>
+        {best}
+        <WPMText />
+      </dd>
+      <dt aria-label="Score History">
+        | <Icon name="clock" />
+      </dt>
+      <dd>
+        <Link
+          to={"/history"}
+          className="active-button best__history text-center"
+        >
+          <Icon name="alarm" />
+          <SROnly>Show history</SROnly>
+        </Link>
+      </dd>
+    </dl>
+  );
+};
