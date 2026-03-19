9063d33d1ea2567c94e6eda04a80b039496dd81b
vickbk <vickbake8@gmail.com>
2026-03-19 22:51:07 +0200

    refactor: extract usePersonalBest hook

--- src/features/header/components/personal-best.tsx
+++ src/features/header/components/personal-best.tsx
@@ -1,36 +1,15 @@
-import { useTypingCtx } from "@/features/typing-speed";
 import { WPMText } from "@/features/typing-speed/components/wpm-text";
-import type { TypeScore } from "@/libs/types/typing-speed-types";
-import { getMemoItem } from "@/shared";
 import { Icon } from "@/shared/helpers/components/bi-icon";
 import { SROnly } from "@/shared/helpers/components/SROnly";
 import { default as bestIcon } from "@assets/images/icon-personal-best.svg";
-import { useCallback } from "react";
 import { Link } from "react-router-dom";
+import { usePersonalBest } from "../hooks";
 
 export const PersonalBest = () => {
-  const {
-    state: { difficulty, best },
-    dispatch,
-  } = useTypingCtx();
-
-  const loadResults = useCallback(
-    (node: HTMLElement | null) => {
-      if (node !== null) {
-        const results = getMemoItem<TypeScore[]>(`score.${difficulty}`) || [
-          { wpm: 0 },
-        ];
-        const [higher] = results.sort(
-          ({ wpm: aWPM }, { wpm: bWPM }) => bWPM - aWPM,
-        );
-        dispatch({ action: "updateHighScore", payload: higher.wpm });
-      }
-    },
-    [difficulty, best],
-  );
+  const { best } = usePersonalBest();
 
   return (
-    <dl className="best" ref={loadResults}>
+    <dl className="best">
       <dt className="capitalize c-secondary flex gap-1">
         <img src={bestIcon} alt="" />
         <span className="sr-only sm:not-sr-only">Personal </span>
