8b699479e7123eb20da79ff36c967312f307f8ae
vickbk <vickbake8@gmail.com>
2026-03-19 15:23:47 +0200

    feat: add useTypingCtx hook for improved usage

--- src/features/typing-speed/hooks/typing-context.ts
+++ src/features/typing-speed/hooks/typing-context.ts
@@ -1,4 +1,10 @@
-import { createContext } from "react";
-import { useTypingSpeed } from "./use-typing-speed";
+import { createContext, useContext } from "react";
+import type { AllOptions, AppState } from "../types";
 
-export const TypingContext = createContext(useTypingSpeed());
+export const TypingContext = createContext(
+  {} as { state: AppState; dispatch: React.ActionDispatch<[AllOptions]> },
+);
+
+export function useTypingCtx() {
+  return useContext(TypingContext);
+}
