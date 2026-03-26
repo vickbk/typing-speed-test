45b868be0dcb845ae3c46e368aae65e2c3b53de2
vickbk <vickbake8@gmail.com>
2026-03-26 10:14:14 +0200

    feat: update useTypingSpeed to optionally accept a default state in case it is provided

--- src/features/typing-speed/hooks/use-typing-speed.ts
+++ src/features/typing-speed/hooks/use-typing-speed.ts
@@ -2,28 +2,31 @@ import { getRandomElement } from "@/shared";
 import { useReducer } from "react";
 import { default as textes } from "../assets/data.json";
 import { handleTypingSpeed } from "../scripts";
-import type { Difficulty } from "../types";
+import type { AppState, Difficulty } from "../types";
 
-export function useTypingSpeed() {
-  const [state, dispatch] = useReducer(handleTypingSpeed, {
-    mode: "",
-    difficulty: "easy",
-    typing: false,
-    text: getRandomElement(
-      textes[
-        getRandomElement<Difficulty>([
-          "easy",
-          "code",
-          "hard",
-          "medium",
-          "quote",
-        ])
-      ],
-    ).text,
-    errorCount: 0,
-    finish: false,
-    best: 0,
-    oldMistakes: "",
-  });
+export function useTypingSpeed(initial: AppState | null = null) {
+  const [state, dispatch] = useReducer(
+    handleTypingSpeed,
+    initial ?? {
+      mode: "",
+      difficulty: "easy",
+      typing: false,
+      text: getRandomElement(
+        textes[
+          getRandomElement<Difficulty>([
+            "easy",
+            "code",
+            "hard",
+            "medium",
+            "quote",
+          ])
+        ],
+      ).text,
+      errorCount: 0,
+      finish: false,
+      best: 0,
+      oldMistakes: "",
+    },
+  );
   return { state, dispatch };
 }
