70cd56afdd986b1fe9725009c1d78878f6bce89f
vickbk <vickbake8@gmail.com>
2026-04-01 09:28:23 +0200

    refactor: use buildInitial state helper instead of declaring the whole state in useTypingSpeed

--- src/features/typing-speed/hooks/use-typing-speed.ts
+++ src/features/typing-speed/hooks/use-typing-speed.ts
@@ -1,32 +1,26 @@
 import { getRandomElement } from "@/shared";
 import { useReducer } from "react";
 import { default as textes } from "../assets/data.json";
-import { handleTypingSpeed } from "../scripts";
+import { buildInitialState, handleTypingSpeed } from "../scripts";
 import type { AppState, Difficulty } from "../types";
 
 export function useTypingSpeed(initial: AppState | null = null) {
   const [state, dispatch] = useReducer(
     handleTypingSpeed,
-    initial ?? {
-      mode: "",
-      difficulty: "easy",
-      typing: false,
-      text: getRandomElement(
-        textes[
-          getRandomElement<Difficulty>([
-            "easy",
-            "code",
-            "hard",
-            "medium",
-            "quote",
-          ])
-        ],
-      ).text,
-      errorCount: 0,
-      finish: false,
-      best: 0,
-      oldMistakes: "",
-    },
+    initial ??
+      buildInitialState({
+        text: getRandomElement(
+          textes[
+            getRandomElement<Difficulty>([
+              "easy",
+              "code",
+              "hard",
+              "medium",
+              "quote",
+            ])
+          ],
+        ).text,
+      }),
   );
   return { state, dispatch };
 }
